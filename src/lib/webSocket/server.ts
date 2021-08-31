d2326b1e132ea085f52d73991d0cf687072157c3
Zach Curtis <zachariahtimothy@gmail.com>
2021-08-31 09:22:43 -0400

    Add node adapter Fix server build

--- src/lib/webSocket/server.ts
+++ src/lib/webSocket/server.ts
@@ -1,24 +1,25 @@
-import { ServerOptions, Server } from 'ws';
+import * as WebSocket from 'ws';
 
-const serverOptions: ServerOptions = {
+const serverOptions: WebSocket.ServerOptions = {
   port: 3001,
 }
-
-
-let wsServer: Server;
+let wsServer: WebSocket.Server;
 
 if (process.env.NODE_ENV === 'development') {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     if (!global._wssServer) {
-      global._wssServer = new Server(serverOptions);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      global._wssServer = new WebSocket.WebSocketServer(serverOptions);
     }
     wsServer = global._wssServer;
 } else {
     // In production mode, it's best to not use a global variable.
-    wsServer = new Server(serverOptions);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    wsServer = new WebSocket.WebSocketServer(serverOptions);
 }
-
 // Export a module-scoped MongoClient promise. By doing this in a
 // separate module, the client can be shared across functions.
 export default wsServer;
